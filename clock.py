4883997ac68f553a6e56fad8d4a95e1973f09887
Joyqul Chen <joyqul.tw@gmail.com>
2020-03-05 08:52:35 +0800

    Add replurk_bl_works to schedule

--- clock.py
+++ clock.py
@@ -1,4 +1,5 @@
 import replurk, quote, ura_pc_gacha_config
+import replurk_bl_works
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
@@ -9,12 +10,20 @@ def timed_job():
     ## temporary closed
     #replurk.replurk_pc_gatch_posts()
 
+
 @sched.scheduled_job('interval', minutes=ura_pc_gacha_config.INTERVAL_MINUTES)
 def timed_job():
     quote.quote_pc_gatch_plurk()
 
+
 @sched.scheduled_job('interval', minutes=30)
 def timed_job():
     replurk.replurk_appraisal_posts()
+
+
+@sched.scheduled_job('interval', hours=1)
+def timed_job():
+    replurk_bl_works.replurk_bl_works()
+    
 
 sched.start()
