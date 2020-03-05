51a1bcb20fb5d565573e375ad059d461589b40d1
Joyqul Chen <joyqul.tw@gmail.com>
2020-03-06 05:02:50 +0800

    Fix quote bot error caused by modulizing ReplurkBot

--- quote.py
+++ quote.py
@@ -2,72 +2,69 @@ import json, datetime, urllib
 import util, replurk
 
 
-def valid_to_replurk(post, config):
-    if post['user_id']!=replurk.ANONYMOUS_ID: return False
-
-    found_must_have_tag = False
-    for tag in config.MUST_HAVE_AT_LEAST_ONE:
-        if post['content'].find(tag)>=0:
-            found_must_have_tag = True
-            break
-    if not found_must_have_tag: return False
-    return True
+class QuoteBot(replurk.ReplurkBot):
+    def valid_to_replurk(self, post):
+        if post['user_id']!=replurk.ANONYMOUS_ID: return False
+        if not self.contain_must_have_tag(post): return False
+        return True
 
 
-def get_candidates_posts(config, secret):
-    timedelta = datetime.timedelta(minutes=config.INTERVAL_MINUTES)
-    posted_time_limit = datetime.datetime.utcnow()-timedelta
-
-    posts = replurk.search_plurk(config, secret)
-    candidates = []
-    for post in posts:
-        if not replurk.valid_to_replurk(post, config): continue
-        posted_time = datetime.datetime.strptime(post['posted'], '%a, %d %b %Y %H:%M:%S %Z')
-        if posted_time_limit>=posted_time>=posted_time_limit-timedelta:
-            candidates.append(post)
-    return candidates
-
-
-def base36encode(number):
-    if not isinstance(number, (int)):
-        raise TypeError('number must be an integer')
-    if number < 0:
-        raise ValueError('number must be positive')
-
-    alphabet, base36 = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', '']
-
-    while number:
-        number, i = divmod(number, 36)
-        base36 = alphabet[i] + base36
-
-    return (base36 or alphabet[0]).lower()
+    def get_candidates_posts(self):
+        timedelta = datetime.timedelta(minutes=self.config.INTERVAL_MINUTES)
+        posted_time_limit = datetime.datetime.utcnow()-timedelta
+        posts = self.search_plurk()
+        candidates = []
+        for post in posts:
+            if not self.valid_to_replurk(post): continue
+            posted_time = datetime.datetime.strptime(post['posted'], '%a, %d %b %Y %H:%M:%S %Z')
+            if posted_time_limit>=posted_time>=posted_time_limit-timedelta:
+                candidates.append(post)
+        return candidates
 
 
-def quote_post(post, secret):
-    client = util.get_authenticated_client(secret)
-    url = 'https://www.plurk.com/APP/Timeline/plurkAdd?'
-    plurk_url_id = base36encode(post['plurk_id'])
-    full_plurk_url = 'https://www.plurk.com/p/'+plurk_url_id
+    def base36encode(number):
+        if not isinstance(number, (int)):
+            raise TypeError('number must be an integer')
+        if number < 0:
+            raise ValueError('number must be positive')
 
-    content = ''
-    if post['publish_to_followers']:
-        content += '**可能為親友粉絲限定**'
-    else:
-        content += '**偷偷說公開河道 only**'
-    content +='\n'+post['content_raw']
-    content += '\n'+full_plurk_url
-    params = urllib.parse.urlencode({'content': content, 'qualifier': 'says'})
-    url = url+params
-    response = client.request(url, method='GET')
+        alphabet, base36 = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', '']
 
-    
+        while number:
+            number, i = divmod(number, 36)
+            base36 = alphabet[i] + base36
+
+        return (base36 or alphabet[0]).lower()
+
+
+    def quote_post(self, post):
+        url = 'https://www.plurk.com/APP/Timeline/plurkAdd?'
+        plurk_url_id = QuoteBot.base36encode(post['plurk_id'])
+        full_plurk_url = 'https://www.plurk.com/p/'+plurk_url_id
+
+        content = ''
+        if post['publish_to_followers']:
+            content += '**可能為親友粉絲限定**'
+        else:
+            content += '**偷偷說公開河道 only**'
+        content +='\n'+post['content_raw']
+        content += '\n'+full_plurk_url
+        params = urllib.parse.urlencode({'content': content, 'qualifier': 'says'})
+        url = url+params
+        response = self.client.request(url, method='GET')
+
+
+    def search_and_quote_posts(self):
+        posts = self.get_candidates_posts()
+        posts.reverse()
+        for post in posts:
+            self.quote_post(post)
+
 def quote_pc_gatch_plurk():
     import ura_pc_gacha_config as config, ura_pc_gacha_secret as secret
-    posts = get_candidates_posts(config, secret)
-    posts.reverse()
-    for post in posts:
-        quote_post(post, secret)
-        
+    bot = QuoteBot(config, secret)
+    bot.search_and_quote_posts()
+
 
 if __name__ == '__main__':
     quote_pc_gatch_plurk()
