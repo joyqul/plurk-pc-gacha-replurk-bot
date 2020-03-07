4d2ebb07fb4da807f58b88a6e2dc49443a58dd36
Joyqul Chen <joyqul.tw@gmail.com>
2020-03-07 20:37:56 +0800

    Add tag 自創bl 台灣bl

--- replurk_bl_works.py
+++ replurk_bl_works.py
@@ -2,7 +2,15 @@ import replurk
 
 
 class ReplurkBlWroksBot(replurk.ReplurkBot):
+    def is_possible_creater_user(self, post):
+        none_creater_user_ids = [
+            5087621, #卡布店小二
+        ]
+        return post['user_id'] not in none_creater_user_ids
+
+
     def valid_to_replurk(self, post):
+        if not self.is_possible_creater_user(post): return False
         if self.contain_must_replurk_tag(post): return True
         if post['user_id']==replurk.ANONYMOUS_ID: return False
         if not self.contain_must_have_tag(post): return False
