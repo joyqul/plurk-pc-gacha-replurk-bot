c66d08a561ee7ef6a3b0772d7e412a3c5cba2bbc
Joyqul Chen <joyqul.tw@gmail.com>
2020-03-08 18:18:24 +0800

    Add skip_tag for appraisal_replurk_bot

--- appraisal_config.py
+++ appraisal_config.py
@@ -1,4 +1,4 @@
-SKIP_TAGS = ["#勿轉", "#機器人不要來", "#機器人走開", "#不要轉", "#機器人勿轉", "#goaway"]
+SKIP_TAGS = ["#勿轉", "#機器人不要來", "#機器人走開", "#不要轉", "#機器人勿轉", "#goaway", "#禁轉"]
 MUST_REPLURK_TAGS = ["轉噗感謝", "幫轉"]
 MUST_HAVE_AT_LEAST_ONE = ["#委託鑑價", "#匿名評圖"]
 SEARCH_KEYWORDS = ["委託鑑價", "匿名評圖"]
