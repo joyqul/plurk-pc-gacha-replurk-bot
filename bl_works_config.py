9bc2d04657af69bdffaf8b2df0abd87bac65f7cc
Joyqul Chen <joyqul.tw@gmail.com>
2020-04-15 21:11:47 +0800

    Update skip tags

--- bl_works_config.py
+++ bl_works_config.py
@@ -1,4 +1,4 @@
-SKIP_TAGS = ["#勿轉", "#不要轉", "噗幣轉蛋", "安價", "售物", "https://www.plurk.com/p/", "心得", "memo", "書單", "推文"]
+SKIP_TAGS = ["#勿轉", "#不要轉", "噗幣轉蛋", "安價", "售物", "https://www.plurk.com/p/", "心得", "memo", "書單", "推文", "#徵友", "#自介"]
 MUST_REPLURK_TAGS = ["轉噗感謝", "幫轉", "轉噗分享"]
 MUST_HAVE_AT_LEAST_ONE = ["原創耽美", "原耽", "bl", "在水裡寫字", "轉噗感謝"]
 SEARCH_KEYWORDS = ["原創bl", "自創bl", "台灣bl"]
